959e66b8a1980ba473ffbc2844759c72c11d88ea
Jesus David Saavedra Manrique <jsaavedra@eticos.com>
2022-01-16 21:29:01 -0500

    Use Breed List

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-
-    React.createElement("h1", {}, "Adopt Me"),
-    React.createElement(Pet, {
-      Contract: "900156264",
-      Name: "NuevaEps",
-      Tipe: "Contribuido",
-    }),
-    React.createElement(Pet, {
-      Contract: "900999864",
-      Name: "Ecopetrol",
-      Tipe: "Subsidiado",
-    }),
-    React.createElement(Pet, {
-      Contract: "900364268",
-      Name: "Fuerzas",
-      Tipe: "Contribuido",
-    })
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Pet = (Proper) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, Proper.Name),
-    React.createElement("h2", {}, Proper.Contract),
-    React.createElement("h2", {}, Proper.Tipe),
-  ]);
+  return (
+    <div>
+      <h2>{Proper.name}</h2>
+      <h3>{Proper.Contract}</h3>
+      <h3>{Proper.Tipe}</h3>
+    </div>
+  );
 };
 
 export default Pet;
