928c9558d70719e45d9e61095c5437ba817b4fab
david-eticos <jsaavedra@eticos.com>
2022-01-25 13:34:47 -0500

    dark orange

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -91,6 +91,7 @@ const SearchParams = () => {
             <option value="chartreuse">Chartreuse</option>
             <option value="mediumorchid">Medium Orchid</option>
             <option value="DarkGray">DarkGray</option>
+            <option value="	DarkOrange">	DarkOrange</option>
           </select>
         </label>
         <button style={{ backgroundColor: theme }}>{console.log(theme)}Submit</button>
